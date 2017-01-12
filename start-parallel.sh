c8aff563cbb204fd0daf9d8c1a362ff78aae38e8
lstrand <leon.strand@medeanalytics.com>
2017-01-12 13:26:38 -0800

    added check for existing containers, images, and volumes before attempting stop and/or remove operations

--- start-parallel.sh
+++ start-parallel.sh
@@ -19,8 +19,27 @@ loop_threshold_logstash=$loop_threshold
 stop_and_remove_all_containers() {
   work() {
     __host=$1
-    echo ssh $__host \''docker stop $(docker ps -aq) && docker rm $(docker ps -aq) && docker rmi $(docker images -f "dangling=true" -q) && docker volume rm $(docker volume ls -qf dangling=true)'\'
-    ssh $__host 'docker stop $(docker ps -aq) && docker rm $(docker ps -aq) && docker rmi $(docker images -f "dangling=true" -q) && docker volume rm $(docker volume ls -qf dangling=true)'
+      containers="$(ssh $__host docker ps -aq)"
+      if [ -n "$containers" ]; then
+        echo
+        echo $0: $__host containers detected\; stopping and removing...
+        echo ssh $__host \''docker stop $(docker ps -aq) && docker rm $(docker ps -aq)'\'
+        ssh $__host 'docker stop $(docker ps -aq) && docker rm $(docker ps -aq)'
+      fi
+      dangling_images="$(ssh $__host docker images -qf dangling=true)"
+      if [ -n "$dangling_images" ]; then
+        echo
+        echo $0: $__host dangling images detected\; removing...
+        echo ssh $__host \''docker rmi $(docker images -f dangling=true -q)'\'
+        ssh $__host 'docker rmi $(docker images -f dangling=true -q)'
+      fi
+      dangling_volumes="$(ssh $__host docker volume ls -qf dangling=true)"
+      if [ -n "$dangling_volumes" ]; then
+        echo
+        echo $0: $__host dangling volumes detected\; removing...
+        echo ssh $__host \''docker volume rm $(docker volume ls -qf dangling=true)'\'
+        ssh $__host 'docker volume rm $(docker volume ls -qf dangling=true)'
+      fi
   }
   for host in $hosts; do
     work $host &
@@ -31,6 +50,12 @@ stop_and_remove_all_containers() {
     echo
     echo ssh $host docker ps -a
     ssh $host docker ps -a
+    echo
+    echo ssh $host docker images -f dangling=true
+    ssh $host docker images -f dangling=true
+    echo
+    echo ssh $host docker volume ls -f dangling=true
+    ssh $host docker volume ls -f dangling=true
   done
 }
 
