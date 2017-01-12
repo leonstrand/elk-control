4487e48fa6d372a5f159c77110cd0cb5dc2ce8ce
lstrand <leon.strand@medeanalytics.com>
2017-01-12 13:47:54 -0800

    initial commit

---
+++ dashboard-elasticsearch-indices.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# leon.strand@medeanalytics.com
+
+
+hosts='
+sacelk101
+sacelk102
+'
+
+for host in $hosts; do
+  if nc -w1 $host 22 </dev/null 2>&1 >/dev/null; then
+    ssh $host "PATH=\$PATH:/usr/sbin; ~/elk/dashboard-elasticsearch-indices.sh"
+    break
+  fi
+done
