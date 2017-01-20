26601c7a9515dc649aa81f0a7f8bbb5ef9540939
lstrand <leon.strand@medeanalytics.com>
2017-01-20 11:11:36 -0800

    initial commit

---
+++ elk-environment-variables-example.sh
@@ -0,0 +1,19 @@
+# example of setting required environment variables in bash
+
+
+# one consul agent per physical elk host
+export CONSUL_AGENTS='
+host1
+host2
+'
+
+# unmodified default
+export CONSUL_PORT=8500
+
+# elk hosts same as consul agents
+export ELK_HOSTS=$CONSUL_AGENTS
+
+# directory of mounted logs from target servers
+export LOGSTASH_DIRECTORY_LOGS=/example_directory
+# unique name to differentiate logstash containers between purposes
+export LOGSTASH_CONTAINER_NAME_PREFIX='logstash-example-prefix'
