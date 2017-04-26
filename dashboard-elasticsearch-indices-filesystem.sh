96baa99b71932c9fa3ce62437f481573a9f9a8ce
lstrand <leon.strand@medeanalytics.com>
2017-04-26 06:48:23 -0700

    changed to show blank line only if more hosts coming

--- dashboard-elasticsearch-indices-filesystem.sh
+++ dashboard-elasticsearch-indices-filesystem.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-hosts=$ELK_HOSTS
-for host in $hosts; do
-  echo
-  echo $host
-  ssh $host du -s /elk/elasticsearch/elasticsearch-?/data/nodes/0/indices/*
-done
+show_indices_filesystem() {
+  while (( "$#" )); do
+    echo $1
+    ssh $1 du -s /elk/elasticsearch/elasticsearch-?/data/nodes/0/indices/*
+    shift
+    [ "$#" -gt 0 ] && echo
+  done 
+}
+show_indices_filesystem $ELK_HOSTS 
