19da2a6ace6d04cf9f9678c71e866b1b8b04f7ed
lstrand <leon.strand@medeanalytics.com>
2017-04-26 06:50:42 -0700

    removed leading spaces

--- dashboard-elasticsearch-indices-filesystem.sh
+++ dashboard-elasticsearch-indices-filesystem.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-  while (( "$#" )); do
-    echo $1
-    ssh $1 du -s /elk/elasticsearch/elasticsearch-?/data/nodes/0/indices/*
-    shift
-    [ "$#" -gt 0 ] && echo
-  done 
+while (( "$#" )); do
+  echo $1
+  ssh $1 du -s /elk/elasticsearch/elasticsearch-?/data/nodes/0/indices/*
+  shift
+  [ "$#" -gt 0 ] && echo
+done
