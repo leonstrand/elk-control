17cc740d42196f99db793bb7a5c5055915b9b40f
lstrand <leon.strand@medeanalytics.com>
2017-04-26 06:50:02 -0700

    changed to take input from comand line

--- dashboard-elasticsearch-indices-filesystem.sh
+++ dashboard-elasticsearch-indices-filesystem.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-show_indices_filesystem() {
   while (( "$#" )); do
     echo $1
     ssh $1 du -s /elk/elasticsearch/elasticsearch-?/data/nodes/0/indices/*
     shift
     [ "$#" -gt 0 ] && echo
   done 
-}
-show_indices_filesystem $ELK_HOSTS 
