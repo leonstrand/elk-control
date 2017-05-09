e8de245da496a56d003debd8f916678d201a83b6
lstrand <leon.strand@medeanalytics.com>
2017-05-09 06:08:16 -0700

    allowed elasticsearch containers greater than 9 filtered out very small indices

--- dashboard-elasticsearch-indices-filesystem.sh
+++ dashboard-elasticsearch-indices-filesystem.sh
@@ -2,7 +2,7 @@
 
 while (( "$#" )); do
   echo $1
-  ssh $1 du -s /elk/elasticsearch/elasticsearch-?/data/nodes/0/indices/*
+  ssh $1 du -s /elk/elasticsearch/elasticsearch-*/data/nodes/0/indices/* | awk '$1 > 100'
   shift
   [ "$#" -gt 0 ] && echo
 done
