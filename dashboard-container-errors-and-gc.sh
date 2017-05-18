f48dfd1fdeaecaf470d68a9894393b3a5c9d878d
lstrand <leon.strand@medeanalytics.com>
2017-05-18 09:23:27 -0700

    made per-container output contingent on having some

--- dashboard-container-errors-and-gc.sh
+++ dashboard-container-errors-and-gc.sh
@@ -5,10 +5,13 @@ for h in $ELK_HOSTS; do
   echo $h
   ssh $ELK_USER@$h '
     for i in $(docker ps -aq); do
-      echo
-      docker ps -af id=$i --format '\''{{.Names}}'\''
-      2>/dev/null docker logs $i | egrep '\''ERROR|gc'\''
-      2>/dev/null docker exec $i date
+      lines="$(2>/dev/null docker logs $i | egrep '\''ERROR|gc'\'')"
+      if [ -n "$lines" ]; then
+        echo
+        docker ps -af id=$i --format '\''{{.Names}}'\''
+        echo "$lines"
+        2>/dev/null docker exec $i date
+      fi
     done | tee >(wc -l)
   '
 done
