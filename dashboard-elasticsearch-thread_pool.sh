96acf90d15a3c0fa37c3772db4f64185347b812a
lstrand <leon.strand@medeanalytics.com>
2017-05-18 18:31:40 -0700

    added thread pool filter to show only queued or rejected

--- dashboard-elasticsearch-thread_pool.sh
+++ dashboard-elasticsearch-thread_pool.sh
@@ -4,12 +4,12 @@
 
 work() {
   __host=$1
-  echo
-  echo $__host
+  #node_name                                        name                active queue rejected
+  #sacelkpai102-elasticsearch-6                     bulk                     0     0        0
   ssh $ELK_USER@$__host '
     work() {
       __port=$1
-      curl -sS $(hostname):$__port/_cat/thread_pool
+      curl -sS $(hostname):$__port/_cat/thread_pool | awk '\''($4 > 0) || ($5 > 0) { print;}'\''
     }
     export -f work
     parallel work ::: $(netstat -lnt | grep :::192 | awk '\''{print $4}'\'' | tr -d :)
