c9e1b89badf70515d825532142fbc1574b1792dc
lstrand <leon.strand@medeanalytics.com>
2017-05-21 17:24:45 -0700

    extended default thread pool output cleaned up

--- dashboard-elasticsearch-thread_pool.sh
+++ dashboard-elasticsearch-thread_pool.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-#time for port in $(
 
 work() {
   __host=$1
-  #node_name                                        name                active queue rejected
-  #sacelkpai102-elasticsearch-6                     bulk                     0     0        0
   ssh $ELK_USER@$__host '
     work() {
       __port=$1
-      curl -sS $(hostname):$__port/_cat/thread_pool | awk '\''($3 > 1) || ($4 > 0) || ($5 > 0) { print;}'\''
+      curl -sS $(hostname):$__port/_cat/thread_pool/?h=node_name,po,name,t,a,ma,q,r | awk '\''($5 > 1) || ($7 > 0) || ($8 > 0) { print}'\''
     }
     export -f work
     parallel work ::: $(netstat -lnt | grep :::192 | awk '\''{print $4}'\'' | tr -d :)
   '
 }
 export -f work
-parallel work ::: $ELK_HOSTS
+parallel work ::: $ELK_HOSTS | sort -V | uniq
