6216b709a521475fe177221dbc1091712fe7c303
lstrand <leon.strand@medeanalytics.com>
2017-05-21 17:30:13 -0700

    removed time-consuming file storage column simplified core to single curl

--- dashboard-elasticsearch-indices.sh
+++ dashboard-elasticsearch-indices.sh
@@ -7,8 +7,20 @@ hosts=$ELK_HOSTS
 user=$ELK_USER
 
 for host in $hosts; do
-  if nc -w1 $host 22 </dev/null 2>&1 >/dev/null; then
-    ssh $user@$host "PATH=\$PATH:/usr/sbin; ~$user/elk/dashboard-elasticsearch-indices.sh"
-    break
-  fi
+  ports=$(ssh $ELK_USER@$host docker ps -f name=elasticsearch --format='{{.Ports}}' | grep 9200)
+  for port in $ports; do
+    case $port in
+      *9200*)
+        # debug: port: 0.0.0.0:19201->9200/tcp,
+        port=$(echo $port | sed 's/^.*:\(.*\)-.*$/\1/')
+        echo curl -sS $host:$port/_cat/indices?v
+        results="$(curl -sS $host:$port/_cat/indices?v)"
+        if [ -n "$results" ]; then
+          echo "$results" | head -1
+          echo "$results" | tail -n +2 | sort -k3
+          exit 0
+        fi
+      ;;
+    esac
+  done
 done
